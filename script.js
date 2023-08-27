742788658d0f618b0c5999165880731d94837c45
MOHAMMAD NOUSHAD <116102583+noushadkas@users.noreply.github.com>
2023-08-28 01:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create a copy of the players array
+const team = [...players];
 
-window.players = players;
-window.person = person;
+// Create another copy of the players array
+const team1 = players.slice();
+
+// Create a copy of the person object
+const cap1 = { ...person };
+
+// Make the variables accessible in the global scope
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
